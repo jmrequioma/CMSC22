ae0a0572882d0d43f7c90aa526249e050b744baf
Jerome Enrico M. Requioma <jerome.requioma@gmail.com>
2016-10-06 08:52:30 +0800

    Update ShapeTest3.java

--- Lab Exercise 8/ShapeTest3.java
+++ Lab Exercise 8/ShapeTest3.java
@@ -6,7 +6,8 @@ public class ShapeTest3 {
 		System.out.println(s1.getPerimeter()); // which version?
 		System.out.println(s1.getColor());
 		System.out.println(s1.isFilled());
-		System.out.println(s1.getRadius());
+		System.out.println(s1.getRadius());   // Shape2 does not have the method getRadius()
+
 		Circle2 c1 = (Circle2)s1; // Downcast back to Circle
 		System.out.println(c1);
 		System.out.println(c1.getArea());
@@ -14,32 +15,36 @@ public class ShapeTest3 {
 		System.out.println(c1.getColor());
 		System.out.println(c1.isFilled());
 		System.out.println(c1.getRadius());
-		Shape2 s2 = new Shape2();
-		Shape2 s3 = new Rectangle2(1.0, 2.0, "RED", false); // Upcast
+		Shape2 s2 = new Shape2();   // this is NOT allowed because Shape2 is abstract and therefore CANNOT be instantiated!!!
 
+		Shape2 s3 = new Rectangle2(1.0, 2.0, "RED", false); // Upcast
 		System.out.println(s3);
 		System.out.println(s3.getArea());
 		System.out.println(s3.getPerimeter());
 		System.out.println(s3.getColor());
-		System.out.println(s3.getLength());
+		System.out.println(s3.getLength());   // Shape2 does not have the method getLength()
+
 		Rectangle2 r1 = (Rectangle2)s3; // downcast
 		System.out.println(r1);
 		System.out.println(r1.getArea());
 		System.out.println(r1.getColor());
 		System.out.println(r1.getLength());
+
 		Shape2 s4 = new Square2(6.6); // Upcast
 		System.out.println(s4);
 		System.out.println(s4.getArea());
 		System.out.println(s4.getColor());
-		System.out.println(s4.getSide());
+		System.out.println(s4.getSide());   // Shape2 does not have the method getSide()
+
 		// Take note that we downcast Shape s4 to Rectangle ,
 		// which is a superclass of Square, instead of Squar e
 		Rectangle2 r2 = (Rectangle2)s4;
 		System.out.println(r2);
 		System.out.println(r2.getArea());
 		System.out.println(r2.getColor());
-		System.out.println(r2.getSide());
+		System.out.println(r2.getSide());   // Rectangle does not have the method getSide()
 		System.out.println(r2.getLength());
+
 		// Downcast Rectangle r2 to Square
 		Square2 sq1 = (Square2)r2;
 		System.out.println(sq1);
